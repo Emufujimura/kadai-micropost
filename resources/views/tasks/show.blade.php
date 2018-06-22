a390562c3e408c8ca9af1f77384aa7d575ec3a03
Emufujimura <emufujimura@gmail.com>
2018-06-22 06:40:51 +0000

    top page

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -3,7 +3,14 @@
 @section('content')
 
     <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
-
+    <div class="container">
+      <div class="row">
+            <div class="col-xs-12"></div>
+            <div class="col-sm-8 col-sm-offset-2"></div>
+            <div class="col-md-8 col-md-offset-2"></div>
+            <div class="col-lg-6 col-lg-offset-3"></div>
+      </div>
+      
       <table class="table table-bordered">
         <tr>
             <th>id</th>
@@ -19,14 +26,12 @@
         </tr>
     </table>
 
-　{!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
+　     {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
 
-   {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
-    {!! Form::close() !!}
+       {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+       {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+       {!! Form::close() !!}
 
+    </div>
 
 @endsection
-
-
-
