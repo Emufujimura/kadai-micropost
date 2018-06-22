6ec75e594d15d1e96034b141ef41a5c5e608ed32
Emufujimura <emufujimura@gmail.com>
2018-06-22 06:12:25 +0000

    bootstrap design

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,14 +3,23 @@
 @section('content')
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
-
+　<div class="container">
+      <div class="row">
+            <div class="col-xs-12"></div>
+            <div class="col-sm-8 col-sm-offset-2"></div>
+            <div class="col-md-8 col-md-offset-2"></div>
+            <div class="col-lg-6 col-lg-offset-3"></div>
+      </div>
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-
+　　　<div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
         {!! Form::text('status') !!}
-       
+       </div>
+
+　　　<div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
+　　　</div>
 
         {!! Form::submit('更新') !!}
 
